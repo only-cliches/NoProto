7bf78ef5b409f79d427f54b6e3841ca08eb1de65
Scott Lott <me@scottlott.com>
2020-11-30 23:21:34 -0800

    cleaning up bench

--- bench/src/main.rs
+++ bench/src/main.rs
@@ -40,37 +40,19 @@ extern crate json;
 
 */
 
-struct TEST<'test> {
-    list: Vec<TEST_LIST<'test>>,
-    initialized: bool,
-    location: &'test str,
-    fruit: u8
-}
+fn main() {
 
-struct TEST_LIST<'test> {
-    name: &'test str,
-    rating: f32,
-    postfix: &'test str,
-    sibling: TEST_LIST_SIBLING<'test>
-}
-
-struct TEST_LIST_SIBLING<'test> {
-    time: u32,
-    ratio: f32,
-    size: u16,
-    parent: TEST_LIST_SIBLING_PARENT<'test>
-}
-
-struct TEST_LIST_SIBLING_PARENT<'test> {
-    id: u64,
-    count: u16,
-    prefix: &'test str,
-    length: u32
-}
-
-fn main() -> Result<(), NP_Error> {
-
+    println!("====== ENCODE BENCHMARK ======");
     
+    no_proto_encode().unwrap();
+    flatbuffers_encode();
+    protobuf_encode();
+
+}
+
+
+pub fn no_proto_encode() -> Result<(), NP_Error> {
+
     let user_factory = NP_Factory::new(r#"{
         "type": "table",
         "columns": [
@@ -99,74 +81,49 @@ fn main() -> Result<(), NP_Error> {
         ]
     }"#)?;
 
-    
-
-    let mut example_data = TEST {
-        initialized: true,
-        location: "https://arstechnica.com",
-        fruit: 2,
-        list: Vec::new()
-    };
-
-    for x in 0..3 {
-        example_data.list.push(TEST_LIST {
-            name: "Hello, world!",
-            rating: 3.1415432432445543543 + (x as f32),
-            postfix: "!",
-            sibling: TEST_LIST_SIBLING {
-                time: 123456 + (x as u32),
-                ratio: 3.14159 + (x as f32),
-                size: 10000 + (x as u16),
-                parent: TEST_LIST_SIBLING_PARENT {
-                    id: 0xABADCAFEABADCAFE + (x as u64),
-                    count: 10000 + (x as u16),
-                    prefix: "@",
-                    length: 1000000 + (x as u32)
-                }
-            }
-        })
-    }
-
     let start = SystemTime::now();
 
     for _x in 0..LOOPS {
 
-
-        // 0.5.0 and greater API
         let mut new_buffer = user_factory.empty_buffer(None, Some(NP_Size::U16));
 
-        new_buffer.fast_insert("initialized", example_data.initialized)?;
-        new_buffer.fast_insert("location", example_data.location)?;
-        new_buffer.fast_insert("fruit", example_data.fruit)?;
+        new_buffer.fast_insert("initialized", true)?;
+        new_buffer.fast_insert("location", "https://arstechnica.com")?;
+        new_buffer.fast_insert("fruit", 2)?;
 
-        for (i, list) in example_data.list.iter().enumerate() {
+        for x in 0..3 {
 
             new_buffer.cursor_to_root();
-            new_buffer.move_cursor(&["list", i.to_string().as_str()])?;
-            new_buffer.fast_insert("name", list.name)?;
-            new_buffer.fast_insert("rating", list.rating)?;
-            new_buffer.fast_insert("postfix", list.postfix)?;
+            new_buffer.move_cursor(&["list", x.to_string().as_str()])?;
+            new_buffer.fast_insert("name", "Hello, world!")?;
+            new_buffer.fast_insert("rating", 3.1415432432445543543 + (x as f32))?;
+            new_buffer.fast_insert("postfix", "!")?;
 
             new_buffer.move_cursor(&["sibling"])?;
-            new_buffer.fast_insert("time", list.sibling.time)?;
-            new_buffer.fast_insert("ratio", list.sibling.ratio)?;
-            new_buffer.fast_insert("size", list.sibling.size)?;
+            new_buffer.fast_insert("time", 123456 + (x as u32))?;
+            new_buffer.fast_insert("ratio", 3.14159 + (x as f32))?;
+            new_buffer.fast_insert("size", 10000 + (x as u16))?;
 
             new_buffer.move_cursor(&["parent"])?;
-            new_buffer.fast_insert("id", list.sibling.parent.id)?;
-            new_buffer.fast_insert("count", list.sibling.parent.count)?;
-            new_buffer.fast_insert("prefix", list.sibling.parent.prefix)?;
-            new_buffer.fast_insert("length", list.sibling.parent.length)?;
+            new_buffer.fast_insert("id", 0xABADCAFEABADCAFE + (x as u64))?;
+            new_buffer.fast_insert("count", 10000 + (x as u16))?;
+            new_buffer.fast_insert("prefix", "@")?;
+            new_buffer.fast_insert("length", 1000000 + (x as u32))?;
             
         }
 
-        assert_eq!(new_buffer.close().len(), 414);
+        assert_eq!(new_buffer.close().len(), 417);
 
         // let bytes = new_buffer.close();
     }
 
     let time = SystemTime::now().duration_since(start).expect("Time went backwards");
-    println!("NoProto:     {:?} size: {} zlib: {}", time, 414, 325);
+    println!("NoProto:     {:?} size: {} zlib: {}", time, 417, 325);
+
+    Ok(())
+}
+
+fn flatbuffers_encode() {
 
     let start = SystemTime::now();
 
@@ -190,12 +147,14 @@ fn main() -> Result<(), NP_Error> {
         let foobarcontainer = FooBarContainerFB::create(&mut fbb, &foobarcontainer_args);
 
         fbb.finish(foobarcontainer, None);
-        assert_eq!(fbb.finished_data().len(), 336);
+        assert_eq!(fbb.finished_data().to_vec().len(), 336);
     }
 
     let time = SystemTime::now().duration_since(start).expect("Time went backwards");
     println!("Flatbuffers: {:?} size: {} zlib: {}", time, 336, 214);
-    
+}
+
+fn protobuf_encode() {
 
     let start = SystemTime::now();
 
@@ -236,11 +195,7 @@ fn main() -> Result<(), NP_Error> {
 
     let time = SystemTime::now().duration_since(start).expect("Time went backwards");
     println!("PBuffers:    {:?} size: {} zlib: {}", time, 220, 163);
-
-    Ok(())
 }
-
-
 
 /*
         // 0.4.2 API
